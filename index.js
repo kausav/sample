d593298d8162e52cfc04152e577e6599effe9275
kausav <kausavkumar23@gmail.com>
2021-08-09 07:07:41 +0530

    new

---
+++ index.js
@@ -0,0 +1,86 @@
+
+
+const readmorebtn = document.querySelector(".readmore-btn");
+const text = document.querySelector(".heading6");
+
+readmorebtn.addEventListener("click", () => {
+  text.classList.toggle("show-more");
+});
+
+// const star1 = document.querySelector(".star1");
+// const star2 = document.querySelector(".star2");
+// const star3 = document.querySelector(".star3");
+// const star4 = document.querySelector(".star4");
+// const star5 = document.querySelector(".star5");
+
+// const p1 = document.querySelector(".p1");
+// const p2 = document.querySelector(".p2");
+// const p3 = document.querySelector(".p3");
+// const p4 = document.querySelector(".p4");
+// const p5 = document.querySelector(".p5");
+
+// function stars1() {
+//   star1.style.color = "#ffe400";
+//   star2.style.color = "#b6b6b6";
+//   star3.style.color = "#b6b6b6";
+//   star4.style.color = "#b6b6b6";
+//   star5.style.color = "#b6b6b6";
+//   p1.style.display="block";
+//   p2.style.display="none";
+//   p3.style.display="none";
+//   p4.style.display="none";
+//   p5.style.display="none";
+
+// }
+
+// function stars2() {
+//   star1.style.color = "#ffe400";
+//   star2.style.color = "#ffe400";
+//   star3.style.color = "#b6b6b6";
+//   star4.style.color = "#b6b6b6";
+//   star5.style.color = "#b6b6b6";
+//   p1.style.display = "none";
+//   p2.style.display = "block";
+//   p3.style.display = "none";
+//   p4.style.display = "none";
+//   p5.style.display = "none";
+// }
+
+// function stars3() {
+//   star1.style.color = "#ffe400";
+//   star2.style.color = "#ffe400";
+//   star3.style.color = "#ffe400";
+//   star4.style.color = "#b6b6b6";
+//   star5.style.color = "#b6b6b6";
+//   p1.style.display = "none";
+//   p2.style.display = "none";
+//   p3.style.display = "block";
+//   p4.style.display = "none";
+//   p5.style.display = "none";
+// }
+
+// function stars4() {
+//   star1.style.color = "#ffe400";
+//   star2.style.color = "#ffe400";
+//   star3.style.color = "#ffe400";
+//   star4.style.color = "#ffe400";
+//   star5.style.color = "#b6b6b6";
+//   p1.style.display = "none";
+//   p2.style.display = "none";
+//   p3.style.display = "none";
+//   p4.style.display = "block";
+//   p5.style.display = "none";
+// }
+
+// function stars5() {
+//   star1.style.color = "#ffe400";
+//   star2.style.color = "#ffe400";
+//   star3.style.color = "#ffe400";
+//   star4.style.color = "#ffe400";
+//   star5.style.color = "#ffe400";
+//   p1.style.display = "none";
+//   p2.style.display = "none";
+//   p3.style.display = "none";
+//   p4.style.display = "none";
+//   p5.style.display = "block";
+// }
